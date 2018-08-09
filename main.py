a5b26058f93ec6bfae009dcc56c5440c5d289e32
Huang <jhuang@webmdhealth.net>
2018-08-09 15:56:38 -0400

    added a check to see if annoucement date match today date

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import praw
 import config
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def get_soup(url):
@@ -14,15 +14,15 @@ def main(bot):
     announcement_list_soup = get_soup('https://www.rayark.com/g/sdorica/en/announcement/')
     announcement_list = announcement_list_soup.find_all('div', class_='list')
     latest_announcement_url = str(announcement_list[0].contents[1]).split('href')[1].split('"')[1]
-    with latest_announcement_url.split('/')[len(latest_announcement_url.split('/'))-2] as latest_announcement_date:
-        if latest_announcement_date == datetime.now().strftime('%Y-%M-%D'):
-            announcement_detail_soup = get_soup('https://www.rayark.com' + latest_announcement_url)
-            announcement_detail_title = announcement_detail_soup.find('span', class_='mainTitle').text
-            announcement_detail_text = announcement_detail_soup.find('p').text.replace('\n', '\n\n')
-            bot.subreddit('reddit_api_test').submit(title=announcement_detail_title,
-                                                    selftext=announcement_detail_text +
-                                                    '\n\nFor any information about the bot pm /u/Core_Skills.'
-                                                    )
+    latest_announcement_date = latest_announcement_url.split('/')[len(latest_announcement_url.split('/'))-3]
+    if latest_announcement_date == (datetime.now() - timedelta(1)).strftime('%Y-%m-%d'):
+        announcement_detail_soup = get_soup('https://www.rayark.com' + latest_announcement_url)
+        announcement_detail_title = announcement_detail_soup.find('span', class_='mainTitle').text
+        announcement_detail_text = announcement_detail_soup.find('p').text.replace('\n', '\n\n')
+        bot.subreddit('reddit_api_test').submit(title=announcement_detail_title,
+                                                selftext=announcement_detail_text +
+                                                '\n\nFor any information about the bot pm /u/Core_Skills.'
+                                                )
 
 
 if __name__ == '__main__':
